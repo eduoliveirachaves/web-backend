598a09e8d245ad1524352e70c0073e1a0b993953
matheusspiccoli <matheuspdmoura@gmail.com>
2025-11-08 12:43:22 -0300

    fix: ProductModule adicionado aos imports do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ProductModule } from '@/product/product.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [UserModule, AuthModule, PrismaModule, ProductModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
