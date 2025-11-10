110c772eee9720e914c0d9c1abdd018fd58c2d41
matheusspiccoli <matheuspdmoura@gmail.com>
2025-11-10 10:57:18 -0300

    fix: Inserção dos imports ProductModule e OrderModule no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ProductModule } from '@/product/product.module';
+import { OrderModule } from '@/order/order.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ProductModule],
+  imports: [UserModule, AuthModule, PrismaModule, ProductModule, OrderModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
