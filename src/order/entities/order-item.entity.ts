dee1e04dafc129c467edb252433447f1b11f4dac
matheusspiccoli <matheuspdmoura@gmail.com>
2025-11-10 10:58:54 -0300

    feat: entity de order-item criada

--- src/order/entities/order-item.entity.ts
+++ src/order/entities/order-item.entity.ts
@@ -1,7 +1,20 @@
-export class OrderItem {
-  id: number;
-  orderId: number;
-  productId: number;
-  amount: number;
-  price: number;
+import { ProductEntity } from '@/product/entities/product.entity';
+import { Expose, Type } from 'class-transformer';
+
+export class OrderItemEntity {
+  @Expose()
+  id: string;
+
+  @Expose()
+  orderId: string;
+
+  @Expose()
+  productId: string;
+
+  @Expose()
+  quantity: number;
+
+  @Expose()
+  @Type(() => ProductEntity)
+  product?: ProductEntity;
 }
