8c817573a28fd6690839ce1ab8f1f25978425516
matheussilvano2005@gmail.com <matheussilvano2005@gmail.com>
2025-11-21 20:55:54 -0300

    feat: habilita CORS no servidor pra conseguir fazer requisições no front

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Carrega as configs do arquivo .env
   const configService = app.get(ConfigService);
 
